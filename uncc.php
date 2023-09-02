ae523cb00905e52e8c6cfd4b34f002e4975b4da9
devakitella <devakitella@gmail.com>
2023-09-01 21:58:45 -0400

    Added new files

---
+++ uncc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC - Devaki</title>
+</head>
+<body>
+    
+    <h1>I am Devaki Tella - 801349509. I am just playing with FE</h1>
+    
+    <h1><?php echo 100* 100?></h1>
+</body>
+</html>
